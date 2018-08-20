2026255bf5677debce9b979c15a21ee2e69db312
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2018-08-20 11:14:54 -0500

    [Add]

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Academy B&F',
+    'category': 'Website',
+    'description': 'Generic controller for web forms',
+    'version': '11.0',
+    'depends': ['base',],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ openacademy/controllers/main.py
@@ -0,0 +1,13 @@
+from odoo import http
+
+class Academy(http.Controller):
+
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        return http.request.render(
+            'openacademy.index',
+            {'teachers': [
+                'Diana Padilla',
+                'Jody Caroll',
+                'Lester Vaughn',
+            ]})
