d8f199ef26099e220df2139b239e949a6c18f9fb
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2018-08-20 16:26:45 -0500

    [ADD] Inherit controllers

--- openacademy/controllers/main.py
+++ openacademy/controllers/main.py
@@ -1,5 +1,7 @@
 from odoo import http
 
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
 class Academy(http.Controller):
 
     @http.route('/academy/academy/', auth='public', website=True)
@@ -13,3 +15,15 @@ class Academy(http.Controller):
     @http.route('/academy/<model("openacademy.teachers"):teacher>', auth='public', website=True)
     def teacher2(self, teacher):
         return http.request.render('openacademy.biografy',{'person':teacher})
+
+class WebsiteSaleInh(WebsiteSale):
+    @http.route()
+    def shop(self, page = 0, category = None, search = "", ppg= False, **post):
+        print("Testing")
+        res = super(WebsiteSaleInh, self).shop(page=page, category = category, search= search, ppg = ppg, **post)
+        res.qcontext['search'] = 'ipad'
+        import ipdb;ipdb.set_trace()
+        res.qcontext['categories'] = res.qcontext['categories'].sorted(key = lambda r:r.name)
+        res.qcontext['products'] = res.qcontext['products'].sorted(key = lambda r:r.name)
+
+        return res
