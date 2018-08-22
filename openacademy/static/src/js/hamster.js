94edb453af9aaa95f84d7dacbb3981cd7e867b6e
Ricardo Jesus Ruiz Cruz <rrcfesc@gmail.com>
2018-08-22 11:05:52 -0500

    [ADD] JS

---
+++ openacademy/static/src/js/hamster.js
@@ -0,0 +1,21 @@
+odoo.define('academy.hamster', function(require) {
+    'use strict';
+    var Animal = require('academy.animal');
+
+
+    var DanceMixin = {
+        dance :  function() {
+            console.log('Dancing ..');
+        }
+    };
+
+
+    var Hamster = Animal.extend(DanceMixin,{
+        sleep: function () {
+            console.log('Sleeping');
+        }
+    });
+    var hamster = new Hamster();
+    hamster.dance();
+    hamster.sleep();
+});
